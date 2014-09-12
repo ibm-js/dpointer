4c051b82254a8068d7cbdb7e5331a950f3969536
Sebastien Pereira <spereira@javalive.net>
2014-09-12 18:08:40 +0200

    make has module runnable/loadable on Node.js

--- handlers/features.js
+++ handlers/features.js
@@ -4,11 +4,13 @@
 define([
 	"requirejs-dplugins/has"
 ], function (has) {
-	has.add("touch-events", "ontouchstart" in document); // UA supports Touch Events
-	has.add("pointer-events", "onpointerdown" in document); // UA supports Pointer Events
-	has.add("mspointer-events", "onmspointerdown" in document); // UA supports Pointer Events (IE10+IE11 preview)
-	has.add("touch-device", /(mobile)|(android)/i.test(navigator.userAgent)); // mobile device
-	has.add("css-touch-action", "touchAction" in document.body.style);// touch-action CSS
-	has.add("css-ms-touch-action", "msTouchAction" in document.body.style);// -ms-touch-action CSS
+	if (typeof document !== "undefined") {
+		has.add("touch-events", "ontouchstart" in document); // UA supports Touch Events
+		has.add("pointer-events", "onpointerdown" in document); // UA supports Pointer Events
+		has.add("mspointer-events", "onmspointerdown" in document); // UA supports Pointer Events (IE10+IE11 preview)
+		has.add("touch-device", /(mobile)|(android)/i.test(navigator.userAgent)); // mobile device
+		has.add("css-touch-action", "touchAction" in document.body.style);// touch-action CSS
+		has.add("css-ms-touch-action", "msTouchAction" in document.body.style);// -ms-touch-action CSS
+	}
 	return has;
 });
